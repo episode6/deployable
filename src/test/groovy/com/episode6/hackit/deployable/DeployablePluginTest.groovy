7ebfb83af9623b7167af1988985f8941d9bf9c2e
Geoff Hackett <ghackett@gmail.com>
2017-01-08 13:06:35 -0500

    add an empty unit test (content to come)

---
+++ src/test/groovy/com/episode6/hackit/deployable/DeployablePluginTest.groovy
@@ -0,0 +1,11 @@
+package com.episode6.hackit.deployable
+
+/**
+ * Tests DeployablePlugin.groovy
+ */
+class DeployablePluginTest extends GroovyTestCase {
+
+  void testEmptyTest() {
+    // empty test to we don't fail the test task
+  }
+}
