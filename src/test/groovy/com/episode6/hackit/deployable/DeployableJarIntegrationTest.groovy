1f6ad59bab82c63141562e375e2694e4ba74a197
Geoff Hackett <ghackett@gmail.com>
2017-01-12 17:30:10 -0500

    starting work on verify maven output test

--- src/test/groovy/com/episode6/hackit/deployable/DeployableJarIntegrationTest.groovy
+++ src/test/groovy/com/episode6/hackit/deployable/DeployableJarIntegrationTest.groovy
@@ -20,6 +20,9 @@ class DeployableJarIntegrationTest extends Specification {
     testProject = new IntegrationTestProject(testProjectDir)
     testProject.createNonEmptyJavaFile( "com", "example", "groupid", "testlib")
     testProject.rootGradlePropertiesFile << testProject.testProperties.getInGradlePropertiesFormat()
+    testProject.rootGradleSettingFile << """
+rootProject.name = 'testlib'
+"""
     testProject.rootGradleBuildFile << """
 plugins {
  id 'java'
@@ -76,5 +79,38 @@ version = '0.0.1-SNAPSHOT'
     result.task(":javadoc").outcome == TaskOutcome.SUCCESS
     result.task(":javadocJar").outcome == TaskOutcome.SUCCESS
     result.task(":sourcesJar").outcome == TaskOutcome.SUCCESS
+  }
+
+  def "verify uploaded files"() {
+    given:
+    File TEMPMAVENDIR = new File("build/m2/snapshot")
+    TEMPMAVENDIR.mkdirs()
+    testProject.rootGradleBuildFile << """
+
+deployable {
+  nexus {
+    snapshotRepoUrl "file://localhost${TEMPMAVENDIR.absolutePath}"
+  }
+}
+"""
+
+    when:
+    def result = GradleRunner.create()
+        .withProjectDir(testProjectDir.root)
+        .withPluginClasspath()
+        .withArguments("deploy")
+        .build()
+
+    then:
+    true
+    File mavenOutputRoot = TEMPMAVENDIR.newFile("com", "example", "groupid", "testlib")
+    File mavenMetaDataXml = mavenOutputRoot.newFile("maven-metadata.xml")
+    def mavenMetaData = new XmlSlurper().parse(mavenMetaDataXml)
+    mavenMetaData.groupId.text() == "com.example.groupid"
+    mavenMetaData.artifactId.text() == "testlib"
+    mavenMetaData.versioning.versions.size() == 1
+    mavenMetaData.versioning.versions.version.text() == "0.0.1-SNAPSHOT"
+    mavenMetaData.versioning.lastUpdated != null
+
   }
 }
