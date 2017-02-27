583e2698755ba6841325d187df49927e48c35d30
Geoff Hackett <ghackett@gmail.com>
2017-02-27 16:07:32 -0500

    make deployableValidationTask a proper VerificationTask

--- src/main/groovy/com/episode6/hackit/deployable/DeployableValidationException.groovy
+++ src/main/groovy/com/episode6/hackit/deployable/DeployableValidationException.groovy
@@ -1,9 +1,11 @@
 package com.episode6.hackit.deployable
 
+import org.gradle.api.GradleException
+
 /**
  * thrown when validateDeployable task fails
  */
-class DeployableValidationException extends RuntimeException {
+class DeployableValidationException extends GradleException {
 
   DeployableValidationException(List<String> missingProperties) {
     super("deployable validation failure - missing required properties: ${missingProperties}")
--- src/main/groovy/com/episode6/hackit/deployable/DeployableValidationTask.groovy
+++ src/main/groovy/com/episode6/hackit/deployable/DeployableValidationTask.groovy
@@ -2,12 +2,16 @@ package com.episode6.hackit.deployable
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.Nullable
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.VerificationTask
 
 /**
  * task that validates deployable project properties
  */
-class DeployableValidationTask extends DefaultTask {
+class DeployableValidationTask extends DefaultTask implements VerificationTask {
+
+  @Input boolean ignoreFailures = false
 
   @TaskAction
   def validate() {
@@ -24,6 +28,14 @@ class DeployableValidationTask extends DefaultTask {
     missingProps.addAll(project.deployable.findMissingProperties())
     if (!missingProps.isEmpty()) {
       throw new DeployableValidationException(missingProps)
+    }
+  }
+
+  private fail(Throwable failure) {
+    if (ignoreFailures) {
+      println failure.message
+    } else {
+      throw failure
     }
   }
 
