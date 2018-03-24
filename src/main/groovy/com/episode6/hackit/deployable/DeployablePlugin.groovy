3971d20ea0e330d67fc657c4ab3b7132f1bc484c
Geoff Hackett <ghackett@gmail.com>
2018-03-24 16:29:17 -0400

    Move pom & signing configuration blocks to the MavenConfig util class

--- src/main/groovy/com/episode6/hackit/deployable/DeployablePlugin.groovy
+++ src/main/groovy/com/episode6/hackit/deployable/DeployablePlugin.groovy
@@ -3,7 +3,6 @@ package com.episode6.hackit.deployable
 import com.episode6.hackit.deployable.extension.DeployablePluginExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.maven.MavenDeployment
 import org.gradle.api.plugins.MavenPlugin
 import org.gradle.plugins.signing.SigningPlugin
 
@@ -56,58 +55,8 @@ class DeployablePlugin implements Plugin<Project> {
           .map("api", "compile")
           .map("mavenProvided", "provided")
 
-      project.uploadArchives {
-        dependsOn project.validateDeployable
-
-        repositories {
-          mavenDeployer {
-            beforeDeployment { MavenDeployment deployment -> project.signing.signPom(deployment) }
-
-            repository(url: deployable.nexus.releaseRepoUrl) {
-              authentication(userName: deployable.nexus.username, password: deployable.nexus.password)
-            }
-            snapshotRepository(url: deployable.nexus.snapshotRepoUrl) {
-              authentication(userName: deployable.nexus.username, password: deployable.nexus.password)
-            }
-
-            pom.project {
-              name project.name
-              packaging pomPackaging
-              description deployable.pom.description
-              url deployable.pom.url
-
-              scm {
-                url deployable.pom.scm.url
-                connection deployable.pom.scm.connection
-                developerConnection deployable.pom.scm.developerConnection
-              }
-
-              licenses {
-                license {
-                  name deployable.pom.license.name
-                  url deployable.pom.license.url
-                  distribution deployable.pom.license.distribution
-                }
-              }
-
-              developers {
-                developer {
-                  id deployable.pom.developer.id
-                  name deployable.pom.developer.name
-                }
-              }
-            }
-          }
-        }
-      }
-
-      project.signing {
-        required {
-          isReleaseBuild(project) &&
-              (project.gradle.taskGraph.hasTask("uploadArchives") ||
-                  project.gradle.taskGraph.hasTask("uploadArchives")) }
-        sign project.configurations.archives
-      }
+      MavenConfig.configurePom(project, deployable, pomPackaging)
+      MavenConfig.configureSigning(project)
 
       OptionalDependencies.applyOptionals(project)
     }
