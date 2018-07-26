4640d6455f528c7a8e046c5256894db63f890ac2
Geoff Hackett <ghackett@gmail.com>
2018-07-26 15:51:12 -0400

    we dont need `afterEvaluate` for tests anymore, yay!

--- src/main/groovy/com/episode6/hackit/deployable/DeployableAarPlugin.groovy
+++ src/main/groovy/com/episode6/hackit/deployable/DeployableAarPlugin.groovy
@@ -8,8 +8,7 @@ import org.gradle.external.javadoc.JavadocMemberLevel
 
 /**
  * Plugin to make Aars (android artifacts) deployable.
- * Referenced as 'com.episode6.hackit.deployable.aar'
- */
+ * Referenced as 'com.episode6.hackit.deployable.aar'*/
 class DeployableAarPlugin implements Plugin<Project> {
   void apply(Project project) {
     DeployablePlugin deployablePlugin = project.plugins.apply(DeployablePlugin)
@@ -19,42 +18,39 @@ class DeployableAarPlugin implements Plugin<Project> {
       artifact project.bundleRelease
     }
 
-    project.afterEvaluate {
+    project.android.libraryVariants.all { variant ->
+      def javadocsTask = project.task("android${variant.name.capitalize()}Javadocs", type: Javadoc) {
+        description "Generates Javadoc for $variant.name."
+        options.memberLevel = JavadocMemberLevel.PRIVATE
+        options.links("http://docs.oracle.com/javase/7/docs/api/");
+        options.links("http://developer.android.com/reference/");
+        exclude '**/BuildConfig.java'
+        exclude '**/R.java'
 
-      project.android.libraryVariants.all { variant ->
-        def javadocsTask = project.task("android${variant.name.capitalize()}Javadocs", type: Javadoc) {
-          description "Generates Javadoc for $variant.name."
-          options.memberLevel = JavadocMemberLevel.PRIVATE
-          options.links("http://docs.oracle.com/javase/7/docs/api/");
-          options.links("http://developer.android.com/reference/");
-          exclude '**/BuildConfig.java'
-          exclude '**/R.java'
+        dependsOn variant.javaCompile
+        mustRunAfter variant.javaCompile
 
-          dependsOn variant.javaCompile
-          mustRunAfter variant.javaCompile
-
-          source = variant.javaCompile.source
-          doFirst {
-            classpath += project.files(project.android.bootClasspath)
-            classpath += project.files(variant.javaCompile.classpath)
-          }
+        source = variant.javaCompile.source
+        doFirst {
+          classpath += project.files(project.android.bootClasspath)
+          classpath += project.files(variant.javaCompile.classpath)
         }
+      }
 
-        def javadocJarTask = project.task("android${variant.name.capitalize()}JavadocsJar", type: Jar, dependsOn: javadocsTask) {
-          classifier = 'javadoc'
-          from javadocsTask
-        }
+      def javadocJarTask = project.task("android${variant.name.capitalize()}JavadocsJar", type: Jar, dependsOn: javadocsTask) {
+        classifier = 'javadoc'
+        from javadocsTask
+      }
 
-        def sourcesJarTask = project.task("android${variant.name.capitalize()}SourcesJar", type: Jar) {
-          classifier = 'sources'
-          from variant.javaCompile.source
-        }
+      def sourcesJarTask = project.task("android${variant.name.capitalize()}SourcesJar", type: Jar) {
+        classifier = 'sources'
+        from variant.javaCompile.source
+      }
 
-        if (variant.name == "release") {
-          project.deployable.publication {
-            artifact javadocJarTask
-            artifact sourcesJarTask
-          }
+      if (variant.name == "release") {
+        project.deployable.publication {
+          artifact javadocJarTask
+          artifact sourcesJarTask
         }
       }
     }
