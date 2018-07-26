f9b28898e94c612b5afa86b58e76a31f11d27918
Geoff Hackett <ghackett@gmail.com>
2018-07-26 16:19:29 -0400

    Make kotlin aar tests pass

--- src/main/groovy/com/episode6/hackit/deployable/DeployableKotlinAarPlugin.groovy
+++ src/main/groovy/com/episode6/hackit/deployable/DeployableKotlinAarPlugin.groovy
@@ -6,57 +6,55 @@ import org.gradle.api.tasks.bundling.Jar
 
 /**
  * Plugin to make Kotlin Jars deployable
- * Referenced as 'com.episode6.hackit.deployable.kt.jar'
- */
+ * Referenced as 'com.episode6.hackit.deployable.kt.jar'*/
 class DeployableKotlinAarPlugin implements Plugin<Project> {
   void apply(Project project) {
     DeployablePlugin deployablePlugin = project.plugins.apply(DeployablePlugin)
     deployablePlugin.pomPackaging = "aar"
 
-    project.afterEvaluate {
-      project.plugins.apply('org.jetbrains.dokka-android')
+    project.plugins.apply('org.jetbrains.dokka-android')
 
-      project.android.sourceSets.each {
-        it.java.srcDirs += "src/${it.name}/kotlin"
+    project.android.sourceSets.each {
+      it.java.srcDirs += "src/${it.name}/kotlin"
+    }
+
+    project.tasks.dokka {
+      doFirst {
+        classpath += project.files(project.android.bootClasspath)
       }
+      externalDocumentationLink {
+        url = new URL("http://developer.android.com/reference/")
+      }
+      reportUndocumented = false
+    }
+
+    project.task("javadocJar", type: Jar, dependsOn: project.dokka) {
+      classifier = 'javadoc'
+      from project.dokka
+    }
+
+    project.deployable.publication {
+      artifact project.bundleRelease
+      artifact project.javadocJar
+    }
+
+    project.android.libraryVariants.all { variant ->
 
       project.tasks.dokka {
-        classpath += project.files(project.android.bootClasspath)
-        externalDocumentationLink {
-          url = new URL("http://developer.android.com/reference/")
+        doFirst {
+          classpath += project.files(variant.javaCompile.classpath)
         }
-        reportUndocumented = false
       }
 
-      project.task("javadocJar", type: Jar, dependsOn: project.dokka) {
-        classifier = 'javadoc'
-        from project.dokka
+      def sourcesJarTask = project.task("android${variant.name.capitalize()}SourcesJar", type: Jar) {
+        classifier = 'sources'
+        from variant.javaCompile.source.collect() + project.android.sourceSets.main.java.srcDirs.collect() +
+            project.android.sourceSets.findByName(variant.dirName)?.java?.srcDirs?.collect()
       }
 
-      project.deployable.publication {
-        artifact project.bundleRelease
-        artifact project.javadocJar
-      }
-
-      project.android.libraryVariants.all { variant ->
-
-        project.tasks.dokka {
-          doFirst {
-            classpath += project.files(variant.javaCompile.classpath)
-          }
-        }
-
-        def sourcesJarTask = project.task("android${variant.name.capitalize()}SourcesJar", type: Jar) {
-          classifier = 'sources'
-          from variant.javaCompile.source.collect() +
-              project.android.sourceSets.main.java.srcDirs.collect() +
-              project.android.sourceSets.findByName(variant.dirName)?.java?.srcDirs?.collect()
-        }
-
-        if (variant.name == "release") {
-          project.deployable.publication {
-            artifact sourcesJarTask
-          }
+      if (variant.name == "release") {
+        project.deployable.publication {
+          artifact sourcesJarTask
         }
       }
     }
