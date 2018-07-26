c930e504cf0be85aab523ebb9e4651a22fd76cf2
Geoff Hackett <ghackett@gmail.com>
2018-07-26 10:25:38 -0400

    convert DeployablePlugin to use maven-publish and update tasks to point to maven-publish tasks

--- src/main/groovy/com/episode6/hackit/deployable/DeployablePlugin.groovy
+++ src/main/groovy/com/episode6/hackit/deployable/DeployablePlugin.groovy
@@ -3,7 +3,7 @@ package com.episode6.hackit.deployable
 import com.episode6.hackit.deployable.extension.DeployablePluginExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.MavenPlugin
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.plugins.signing.SigningPlugin
 
 /**
@@ -20,7 +20,7 @@ class DeployablePlugin implements Plugin<Project> {
   }
 
   void apply(Project project) {
-    project.plugins.apply(MavenPlugin)
+    project.plugins.apply(MavenPublishPlugin)
     project.plugins.apply(SigningPlugin)
 
     mavenConfig = new MavenConfigurator(project: project)
@@ -41,14 +41,22 @@ class DeployablePlugin implements Plugin<Project> {
     }
     
     // add deploy alias for uploadArchives task (because it's more fun to type)
-    project.task("deploy", dependsOn: project.uploadArchives) {
-      description = "A simple alias for uploadArchives, because it's more fun to say."
-      group = "upload"
+    project.task("deploy", dependsOn: project.publish) {
+      description = "A simple alias for publish, because it's more fun to say."
+      group = "publishing"
+    }
+
+    // add install alias for publishToMavenLocal to maintain compatibility with old versions of deployable
+    project.task('install', dependsOn: project.publishToMavenLocal) {
+      description = 'A simple alias for publishToMavenLocal to maintain compatibility with old versions of deployable.'
+      group = 'publishing'
     }
 
     project.afterEvaluate {
       mavenConfig.configure(deployable, pomPackaging)
-      project.tasks.findByPath("install")?.dependsOn project.validateDeployable
+      // TODO fix tasks
+      project.tasks.findByPath("publishMavenArtifactsPublicationToRepository")?.dependsOn project.validateDeployable
+      project.tasks.findByPath("publishMavenArtifactsPublicationToMavenLocal")?.dependsOn project.validateDeployable
       project.tasks.findByPath("check")?.dependsOn project.validateDeployable
       project.tasks.findByPath("test")?.dependsOn project.validateDeployable
     }
