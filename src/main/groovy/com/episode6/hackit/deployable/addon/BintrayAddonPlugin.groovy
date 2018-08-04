71ba820eaacd7451c95eb57d2cd4d4a04892d5f3
Geoff Hackett <ghackett@gmail.com>
2018-08-04 10:11:11 -0400

    add an empty bintray plugin that just applies bintray's plugin

---
+++ src/main/groovy/com/episode6/hackit/deployable/addon/BintrayAddonPlugin.groovy
@@ -0,0 +1,21 @@
+package com.episode6.hackit.deployable.addon
+
+import org.gradle.api.Incubating
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Plugin that applies the bintray plugin for you and handles some of the default configuration
+ * for data that is already present in deployable's config
+ *
+ * Referenced as 'com.episode6.hackit.deployable.addon.bintray'
+ */
+@Incubating
+class BintrayAddonPlugin implements Plugin<Project> {
+
+  @Override
+  void apply(Project project) {
+    project.plugins.apply('com.jfrog.bintray')
+  }
+
+}
---
+++ src/test/groovy/com/episode6/hackit/deployable/addon/BintrayAddonTest.groovy
@@ -0,0 +1,60 @@
+package com.episode6.hackit.deployable.addon
+
+import com.episode6.hackit.deployable.testutil.IntegrationTestProject
+import com.episode6.hackit.deployable.testutil.MavenOutputVerifier
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Rule
+import spock.lang.Specification
+
+/**
+ * Tests the {@link BintrayAddonPlugin}
+ */
+class BintrayAddonTest extends Specification {
+
+  private static String simpleBuildFile(String groupId, String versionName) {
+    return """
+plugins {
+ id 'java-library'
+ id 'com.episode6.hackit.deployable.jar'
+ id 'com.episode6.hackit.deployable.addon.bintray'
+}
+
+group = '${groupId}'
+version = '${versionName}'
+ """
+  }
+
+  @Rule final IntegrationTestProject testProject = new IntegrationTestProject()
+
+  def "verify jar deploy tasks and output"(String groupId, String artifactId, String versionName) {
+    given:
+    testProject.rootProjectName = artifactId
+    testProject.rootGradlePropertiesFile << testProject.testProperties.inGradlePropertiesFormat
+    testProject.rootGradleBuildFile << simpleBuildFile(groupId, versionName)
+    testProject.createNonEmptyJavaFile("${groupId}.${artifactId}")
+    MavenOutputVerifier mavenOutputVerifier = new MavenOutputVerifier(
+        groupId: groupId,
+        artifactId: artifactId,
+        versionName: versionName,
+        testProject: testProject)
+
+    when:
+    def result = testProject.executeGradleTask("deploy", '--stacktrace')
+
+    then:
+    result.task(":jar").outcome == TaskOutcome.SUCCESS
+    result.task(":javadoc").outcome == TaskOutcome.SUCCESS
+    result.task(":javadocJar").outcome == TaskOutcome.SUCCESS
+    result.task(":sourcesJar").outcome == TaskOutcome.SUCCESS
+    result.task(":validateDeployable").outcome == TaskOutcome.SUCCESS
+    result.task(":signMavenArtifactsPublication").outcome == TaskOutcome.SUCCESS
+    result.task(":publishMavenArtifactsPublicationToMavenRepository").outcome == TaskOutcome.SUCCESS
+    result.task(":deploy").outcome == TaskOutcome.SUCCESS
+    mavenOutputVerifier.verifyStandardOutput()
+
+    where:
+    groupId                 | artifactId    | versionName
+    "com.snapshot.example"  | "snapshotlib" | "0.0.1-SNAPSHOT"
+    "com.release.example"   | "releaselib"  | "0.0.2"
+  }
+}
